65c2c02ca2e77d6e11fccc76430207dc76137e41
EungBug <eungb14@gmail.com>
2023-11-03 15:31:26 +0900

    Style: 모듈 css로 Style 수정하는 방법

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,21 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react'
+
+import styles from './NavBar.module.css';
 
 const NavBar = () => {
   const router = useRouter();
   console.log(router);
   return (
     <nav>
-      <Link href='/' style={{ color: router.pathname === '/' ? 'red' : 'blue'}}>Home</Link>
-      <Link href='/about' style={{ color: router.pathname === '/about' ? 'red' : 'blue'}}>About</Link>
+      <Link href='/' className={[
+        styles.link,
+        router.pathname === '/' ? styles.active : ''
+      ].join(' ')}>Home</Link>
+      <Link href='/about' className={[
+        styles.link,
+        router.pathname === '/about' ? styles.active : ''
+      ].join(' ')}>About</Link>
     </nav>
   )
 }
