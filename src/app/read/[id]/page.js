ea9647a432c7bdf208bd6ac9e0bfb381da0f9999
EungBug <eungb14@gmail.com>
2023-11-04 17:50:45 +0900

    Docs: 데이터 읽어서 읽기 페이지 구현

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,13 @@
-const Read = (props) => {
+// 사용자와 상호관계가 없는 페이지로 서버 컴포넌트로 생성한다.
+
+const Read = async (props) => {
+  const res = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await res.json();
+
   return (
     <>
-      <h2>Read </h2>
-      {props.params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 };
