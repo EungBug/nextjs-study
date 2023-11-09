a66ac074ec2b8f162d1919475c763b0ee6e887da
EungBug <eungb14@gmail.com>
2023-11-09 14:30:29 +0900

    Docs: Redirect Study

--- dreamcoding_nextjs/next.config.js
+++ dreamcoding_nextjs/next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
         hostname: 'images.unsplash.com'
       }
     ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever', // 어느 경로로 들어온 경우
+        destination: '/products', // 이 경로로 리다이렉트 하겠다.
+        permanent: true // 308 code와 함께 캐시하도록 설정
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false 
+      },
+    ]
   }
 }
 
